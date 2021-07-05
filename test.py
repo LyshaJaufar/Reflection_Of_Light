ee14febc1733868102b55d2e345ff12e1c90e23e
LyshaJaufar <lysha.ahmd@gmail.com>
2021-07-05 18:29:50 +0500

    upd

--- test.py
+++ test.py
@@ -1,20 +1,20 @@
-import pygame as pg
-import pygame.gfxdraw
-import pygame.freetype
 import pygame_gui as pgui
 from tkinter import *
 from tkinter.filedialog import asksaveasfilename
+import pygame as pg
+import pygame.gfxdraw
+import pygame.freetype
 
 pg.init()
 pg.freetype.init()
 
-SW, SH = 1280, 690
+SW, SH = 1280, 690;
 window = pg.display.set_mode((SW, SH))
 ui_manager = pgui.UIManager((SW, SH))
-pg.display.set_caption("Reflection of Light")
+pg.display.set_caption("Reflection of Light Simulation")
 
-#logo = pg.image.load("assets/logo.png")
-#pg.display.set_icon(logo)
+# logo = pg.image.load("assets/logo.png")
+# pg.display.set_icon(logo)
 
 GREYBLUE = "#111e42"
 RED = (255, 0, 0)
@@ -25,14 +25,15 @@ class Canvas:
         self.width = size[0]
         self.height = size[1]
         self.display_size = display_size
-        self.sPos = ((SW - self.width) // 2, (SH - self.height) // 2)
-        self.dsPos = ((SW - self.display_size[0])//2 + 10, (SH-self.display_size[1])//2)
+        self.sPos = ((SW - self.width) // 2, (SH - self.height) // 2)       # position of the surface
+        self.dsPos = (SW-display_size[0], (SH-self.display_size[1])//2)    # position of the display window
         self.canvas = pg.Surface((self.width, self.height))
         self.canvas.fill((255, 255, 255))
         self.display_canvas = pg.Surface(self.display_size)
 
     def draw(self, window):
         window.blit(self.display_canvas, self.dsPos)
+
 
 c1 = Canvas((3840, 2160), (int(SW//1.8), int(SH//1.8)))
 
@@ -45,13 +46,16 @@ while run:
             run = False
             break
 
+
+        if event.type == pg.USEREVENT:
+            c1.blit_to_canvas()
+
         ui_manager.process_events(event)
 
     ui_manager.update(delta_time)
     window.fill(background_color)
     c1.draw(window)
     ui_manager.draw_ui(window)
-
     pg.display.update()
 
 pg.quit()
